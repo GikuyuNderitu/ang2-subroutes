07ba398d0fa220952d2b9841c5e470748e2bcd84
Gikuyu Nderitu <gikyunderitu@gmail.com>
2017-06-13 13:18:16 -0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NotesComponent } from './notes/notes.component';
+import { NotesPublicComponent } from './notes/notes-public/notes-public.component';
+import { NotesPersonalComponent } from './notes/notes-personal/notes-personal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotesComponent,
+    NotesPublicComponent,
+    NotesPersonalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/notes/notes-public/notes-public.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-public',
+  templateUrl: './notes-public.component.html',
+  styleUrls: ['./notes-public.component.sass']
+})
+export class NotesPublicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
